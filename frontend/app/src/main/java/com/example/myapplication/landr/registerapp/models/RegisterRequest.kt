7cb281364361ca5aa91feadc7dedffe07445d62a
Brian.Z <18510901233@163.com>
2025-08-22 12:32:47 +1000

    Add user registration and login functionality

---
+++ frontend/app/src/main/java/com/example/myapplication/landr/registerapp/models/RegisterRequest.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication.landr.registerapp.models
+
+data class RegisterRequest(val first_name: String, val last_name: String, val user_name: String, val gender: String, val age: Int, val password: String)
