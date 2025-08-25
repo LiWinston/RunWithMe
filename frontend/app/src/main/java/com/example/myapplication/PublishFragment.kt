f320dc7a88f6214661dc223415f8e244ac63c2ac
WL <yongchunl@student.unimelb.edu.au>
2025-08-25 22:46:12 +1000

    删除多余fe

--- frontend/app/src/main/java/com/example/myapplication/PublishFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication
-
-import androidx.fragment.app.Fragment
-
-class PublishFragment : Fragment(R.layout.fragment_publish)
--- frontend/app/src/main/java/com/example/myapplication/landr/loginapp/models/LoginRequest.kt
+++
@@ -1,2 +0,0 @@
-package com.example.myapplication.landr.loginapp.models
-data class LoginRequest(val username: String, val password: String)
