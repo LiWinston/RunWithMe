45230bf99482daddddb1ddc4ae4cf4bdcf7b944e
WL <yongchunl@student.unimelb.edu.au>
2025-09-10 14:26:16 +1000

    Delete WorkoutRepository.java

--- backend/rwm/src/main/java/com/rwm/repository/WorkoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rwm.repository;
-
-import com.rwm.entity.Workout;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long> {
-    List<Workout> findByUserId(Long userId);
-}
