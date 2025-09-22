211c3487f1bf61bec30b85f1a3e576f022bf798a
MichelleRenyx <yuxinr1@student.unimelb.edu.au>
2025-09-22 15:58:47 +1000

    feat: update start button

--- app/src/main/java/com/example/myapplication/record/StartRecordFragment.kt
+++ app/src/main/java/com/example/myapplication/record/StartRecordFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
@@ -16,7 +15,7 @@ class StartRecordFragment : Fragment(R.layout.fragment_start_record) {
         super.onViewCreated(view, savedInstanceState)
 
         // 开始按钮 → 跳到 PreRecordActivity（倒计时）
-        val btnRecord = view.findViewById<Button>(R.id.btn_record)
+        val btnRecord = view.findViewById<ImageButton>(R.id.btn_record)
         btnRecord.setOnClickListener {
             val intent = Intent(requireContext(), PreRecordActivity::class.java)
             startActivity(intent)
